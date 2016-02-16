9f3aa03513b28053f353b6170a29cc555fe49382
Steven Cassidy <steven.cassidy@gmail.com>
2016-02-16 07:51:18 -0500

    Add initial task display

--- todolist/ViewController.swift
+++ todolist/ViewController.swift
@@ -8,11 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+  var tasks = NSArray()
+
+  @IBOutlet weak var tableView: UITableView!
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return tasks.count;
+  }
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("cell")!
+    let description = String(tasks[indexPath.row].objectForKey("description")!)
+    cell.textLabel?.text = description
+    return cell
+  }
+
+  func showJSON(data:NSData) {
+    do {
+      let jsonResult = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers) as! NSArray
+      tasks = jsonResult
+      dispatch_async(dispatch_get_main_queue(), { () -> Void in
+        self.tableView.reloadData()
+      })
+    } catch {
+      print("Fetch failed: \((error as NSError).localizedDescription)")
+    }
+  }
+
+  func loadTasks() {
+    let url = NSURL(string:"http://localhost:3000/tasks")!
+    print("Will call \(url)")
+    let session = NSURLSession.sharedSession()
+    let task = session.dataTaskWithURL(url) { (data, response, error) -> Void in
+      self.showJSON(data!)
+    }
+    task.resume()
+  }
+
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    loadTasks()
   }
 
   override func didReceiveMemoryWarning() {
